c68edb082bd5708519b4cd79b2591189c4a35e86
xHenrique22 <henrique.oliveira9823@gmail.com>
2026-09-17 20:18:36 -0300

    add list for

--- ADS_Aulas/Python/for.py
+++ ADS_Aulas/Python/for.py
@@ -1,3 +1,4 @@
+#PRIMEIRO USO
 texto = "Teste de uso: aaasdiadaw7dyaaw"
 letra_para_contar = "a"
 contador = 0
@@ -6,3 +7,11 @@ for letra in texto:
     if letra == letra_para_contar:
         contador += 1
 print(f"A letra aparece {contador} vezes")
+
+#LISTA COM FOR
+numeros = [1, 2, 3, 4, 5, 5]
+soma = 0
+
+for numero in numeros:
+    soma += numero
+print(soma)

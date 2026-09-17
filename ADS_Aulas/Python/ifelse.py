27caa0c6caf8fa25fa295d39a8d3a0309d4c28ce
xHenrique22 <henrique.oliveira9823@gmail.com>
2026-09-17 19:48:58 -0300

    add elif

--- ADS_Aulas/Python/ifelse.py
+++ ADS_Aulas/Python/ifelse.py
@@ -1,5 +1,7 @@
 idade = int(input("Insira a idade da criança: "))
 if idade < 5:
     print("Vacine sua criança")
+elif idade == 5:
+    print("Verifique com o médico")
 else:
     print("Não vacine sua criança")
